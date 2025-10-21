54ed6b038b47d03d5236bec1560a0f6e57f631f8
Ashish <ashishyadav4978@gmail.com>
2025-10-21 23:47:47 +0530

    CoustomReact1.0

---
+++ customReact/cusReact.js
@@ -0,0 +1,31 @@
+function customRender(reactElement, Container) {
+
+    /*
+    const domElemnt = document.createElement(reactElement.type)
+    domElemnt.innerHTML = reactElement.children
+
+    //Problem will be created here, What if more than one or two attribute is used so i can't me manual.
+
+    domElemnt.setAttribute('href', reactElement.props.href)
+    domElemnt.setAttribute('target', reactElement.props.target)
+
+    Container.appendChild(domElemnt )
+    */
+
+}
+
+
+const reactElement = {
+    type: "a",
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click here to visit Google'
+}
+
+
+const elementSelect = document.querySelector('#root')
+
+customRender(reactElement, elementSelect)
+
